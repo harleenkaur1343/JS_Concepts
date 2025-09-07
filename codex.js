2e47c83060e299d36382d7bb5305288e84c89940
Harleen Kaur <harleenkaur1343@gmail.com>
2025-09-08 05:28:13 +0530

    second commit

--- codex.js
+++ codex.js
@@ -1,20 +1,12 @@
-const obj = {
+const person1 = {
   name: "Harleen",
-  outer() {
-    console.log("outer this:", this.name); //Harleen
-    //in here this = obj
-    function inner() {
-      console.log("inner this:", this.name);
-    }
-    inner(); //no object calling tabi empty refers to windows.name
-    //use arrow functions
-    arrowInner = () => {
-      console.log("Name from arrow inner: ", this.name);
-    };
-    arrowInner();
-    //defined inside outer - this = obj
-    //Name from arrow inner:  Harleen
+  greet() {
+    console.log("Hello, " + this.name);
   },
 };
 
-obj.outer();
+const person2 = { name: "John" };
+
+person1.greet.call(person2);
+
+//call - called immediately
